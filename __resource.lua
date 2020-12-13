c3d873f8cb370451738c06931e93c13eaf780906
BielCOP <biel_cop@live.com>
2020-12-13 07:07:54 -0300

    First Commit

---
+++ __resource.lua
@@ -0,0 +1,5 @@
+description 'Object Concealer'
+version '0.1'
+
+server_script 'object-concealer.lua'
+client_script 'object-concealer.lua'
---
+++ server/object-concealer.lua
@@ -0,0 +1,78 @@
+RegisterServerEvent('server:createObject')
+RegisterServerEvent('server:loadObjects')
+RegisterServerEvent('server:loadObjectsToPlayer')
+
+-- [[ Variáveis ]]
+
+Project = 'Object Concealer - v0.1'
+Archive = 'objetos.ini'
+
+local objectData = {}
+
+-- [[ Eventos ]]
+
+AddEventHandler('server:createObject', function(model, coords, time) -- vector4 coords
+	table.insert(objectData, {
+		model = model,
+		coords = coords,
+		time = (os.time() + time)
+	})
+	TriggerClientEvent('client:createObject', -1, model, coords) -- Chamado para todos os players
+end)
+
+AddEventHandler('server:loadObjects', function()
+	local f = io.open(Archive, 'r')
+
+	if f then
+		for x in f:lines() do
+			local obj = json.decode(x)
+
+			table.insert(objectData, {
+				model = obj.model,
+				coords = vector4(obj.coords.x, obj.coords.y, obj.coords.z, obj.coords.w),
+				time = obj.time
+			})
+			print('[' .. Project .. ']' .. #objectData .. ' carregado com sucesso!')
+		end
+	end
+end)
+
+AddEventHandler('server:loadObjectsToPlayer', function()
+	local src = source
+
+	for k, v in pairs(objectData) do
+		TriggerClientEvent('client:loadObject', src, v.model, v.coords)
+	end
+end)
+
+-- [[ Threads ]]
+
+Citizen.CreateThread(function()
+	TriggerEvent('server:loadObjects')
+
+	while true do
+		Citizen.Wait(1000)
+
+		for k, v in pairs(objectData) do
+			if v.time < os.time() then
+				TriggerClientEvent('client:deleteObject', -1, v.model, v.coords, 1.0) -- Chamado para todos os players
+
+				objectData[k] = nil
+			end
+		end
+
+		_saveObjects()
+	end
+end)
+
+-- [[ Funções ]]
+
+function _saveObjects() -- dini times
+	local f = io.open(Archive, 'w+')
+
+	for k, v in pairs(objectData) do
+		f:write(json.encode(v) .. '\n')
+	end
+
+	f:close()
+end
